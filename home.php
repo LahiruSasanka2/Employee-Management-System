7c89bde906acbd48edbcd10515c81f1fe09cc46b
LahiruSasanka2 <lahirusasanka2015@gmail.com>
2019-02-23 02:54:51 +0530

    V-1.1.0

--- home.php
+++ home.php
@@ -15,7 +15,7 @@
     <meta content="" name="keywords">
     <meta content="" name="description">
     <link href="img/new/logo.png" rel="icon">
-    <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="img/new/logo.png" rel="apple-touch-icon">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
     <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
